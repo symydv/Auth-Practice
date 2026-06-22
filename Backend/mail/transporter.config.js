cd2285d5c83db1abd6725084ffac5cb63766be1e
symydv <623175@student.nitandhra.ac.in>
2026-06-22 15:37:55 +0530

    fix: update nodemailer configuration to use explicit host and port settings

--- Backend/mail/transporter.config.js
+++ Backend/mail/transporter.config.js
@@ -1,12 +1,17 @@
 import nodemailer from "nodemailer"
 import "dotenv/config";
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  }
-})
+  },
+
+  family: 4, // force IPv4
+});
 
 transporter.verify((err) => {
 
