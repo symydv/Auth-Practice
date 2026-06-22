9838fd07fc516cc7e5029f2323523b262b1aa4be
symydv <623175@student.nitandhra.ac.in>
2026-06-22 15:10:40 +0530

    fix: update API_URL to use environment variable for development and production

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -3,7 +3,7 @@
 import {create} from "zustand"
 import axios from "axios"
 
-const API_URL = "http://localhost:3000/api/auth"
+const API_URL = import.meta.env.MODE === 'development'? "http://localhost:3000/api/auth" : "/api/auth";
 axios.defaults.withCredentials = true; //to send cookies
 
 export const useAuthStore = create((set) => ({
